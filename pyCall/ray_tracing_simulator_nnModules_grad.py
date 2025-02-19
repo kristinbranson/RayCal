6a089ab632e4b8c8064a1dcc5222d8158147de63
AniketRavan <aniket.lnl@gmail.com>
2025-02-19 14:59:32 -0500

    Copied the most recent version of ray_tracing_simulator_nnModules_grad.py library to pyCall

--- pyCall/ray_tracing_simulator_nnModules_grad.py
+++ pyCall/ray_tracing_simulator_nnModules_grad.py
@@ -13,7 +13,7 @@ import os
 import scipy.io as sio
 import torch.nn as nn
 from utils import euclidean_distance
-#mpl.use('TkAgg') # Use this if working on the PC
+mpl.use('TkAgg') # Use this if working on the PC
 #mpl.use('QtAgg') # Use this if working remotely with NoMachine
 plt.ion()
 
