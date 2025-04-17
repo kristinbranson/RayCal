247255fb668abb8572296b2a87027095695f0e63
AniketRavan <aniket.lnl@gmail.com>
2025-04-17 16:01:14 -0400

    Fixed indexing error in computing epipolar lines

--- pyCall/return_projected_ray_two_cameras_prism.py
+++ pyCall/return_projected_ray_two_cameras_prism.py
@@ -11,15 +11,17 @@ from ray_tracing_simulator_nnModules_grad import get_rot_mat
 import yaml
 
 #%%
-print('Reading from config file')
-config_path = 'config.yaml'
-with open(config_path, 'r') as f:
-    data = yaml.safe_load(f)
+#config_path = 'config.yaml'
+#with open(config_path, 'r') as f:
+#    data = yaml.safe_load(f)
 
-dividing_col = config['dividing_column'] # Read dividing path from a yaml path
-rotmat = config['rotmat']
+#dividing_col = config['dividing_column'] # Read dividing path from a yaml path
+#dividing_col = 1336 - 1
+#image_width = 1920
+rotmat = 1
+#rotmat = config['rotmat']
 
-print(f'Loading model from: {PATH}')
+#print(f'Loading model from: {PATH}')
 checkpoint = torch.load(PATH, weights_only=True)
 arena = Arena_reprojection_loss_two_cameras_prism_grid_distances(
             principal_point_pixel_cam_0=torch.tensor([0.,0.]).to(torch.float64),
@@ -57,8 +59,8 @@ def get_annotations_curve(arena, user_annotation,
             arena.stereo_camera_angles[2],
             )
         
-        annotations_curve_real = camera_real.reproject(torch.tensor(r).to(torch.float64), R1, T1)
-        annotations_curve_virtual = camera_virtual.reproject(torch.tensor(r).to(torch.float64), R_stereo_cam, arena.T_stereo_cam)
+        annotations_curve_real = camera_real.reproject(r, R1, T1)
+        annotations_curve_virtual = camera_virtual.reproject(r, R_stereo_cam, arena.T_stereo_cam)
         annotations_curve_real = camera_real.distort_pixels_classical(
                                                             annotations_curve_real,
                                                             cam_real_dist_coeff)
@@ -125,16 +127,14 @@ def get_epipolar_line(arena, user_annotation,
             direction = emergent_ray.direction[:,0][:,None]
             s = torch.linspace(0, 8, 50).to(torch.float64)
             r = origin + s[None, :] * direction        
-            
-            annotations_curve_unlabelled_camera = unlabelled_camera.reproject(torch.tensor(r).to(torch.float64), R_unlabelled, T_unlabelled)
-            annotations_curve_labelled_camera = labelled_camera.reproject(torch.tensor(r).to(torch.float64), R_labelled, T_labelled)
+            annotations_curve_unlabelled_camera = unlabelled_camera.reproject(r, R_unlabelled, T_unlabelled)
+            annotations_curve_labelled_camera = labelled_camera.reproject(r, R_labelled, T_labelled)
             annotations_curve_unlabelled_camera = unlabelled_camera.distort_pixels_classical(
                                                                 annotations_curve_unlabelled_camera,
                                                                 cam_unlabelled_dist_coeff)
             annotations_curve_labelled_camera = labelled_camera.distort_pixels_classical(
                                                                 annotations_curve_labelled_camera,
                                                                 cam_labelled_dist_coeff)
-            print(annotations_curve_labelled_camera[:,0])
             return annotations_curve_unlabelled_camera, annotations_curve_labelled_camera
     
 
@@ -147,9 +147,9 @@ def get_epipolar_line(arena, user_annotation,
             origin = cam_1_ray.origin[:,0][:,None]
             direction = cam_1_ray.direction[:,0][:,None]
             s = torch.linspace(prism_distance, prism_distance + 10, 50).to(torch.float64)
-            r = origin + s[None, :] * direction        
+            r = origin + s[None, :] * direction       
             
-            annotations_curve_unlabelled_camera = unlabelled_camera.reproject(torch.tensor(r).to(torch.float64), R_unlabelled, T_unlabelled)            
+            annotations_curve_unlabelled_camera = unlabelled_camera.reproject(r, R_unlabelled, T_unlabelled)            
             annotations_curve_unlabelled_camera = unlabelled_camera.distort_pixels_classical(
                                                                 annotations_curve_unlabelled_camera,
                                                                 cam_unlabelled_dist_coeff)
@@ -171,22 +171,33 @@ def get_secondary_camera(arena):
                                 arena.T_stereo_cam,
                                 r1=arena.stereocam_r1,
                                 radial_dist_coeffs=arena.radial_dist_coeffs_cam_1)
-    
+
+
 if not(type(user_annotation) == torch.Tensor):
     user_annotation = torch.tensor(user_annotation).to(torch.float64)[:, None]
 
 if rotmat:
-    theta = torch.pi / 2
-    R_theta = torch.tensor([[torch.cos(-theta), -torch.sin(-theta)], [torch.sin(-theta), torch.cos(-theta)]]).to(torch.float64)
-    user_annotation = R_theta @ user_annotation
+    theta = torch.tensor(torch.pi / 2)
+    R_theta_inv = torch.tensor([[torch.cos(-theta), -torch.sin(-theta)], [torch.sin(-theta), torch.cos(-theta)]]).to(torch.float64)
+    R_theta = torch.tensor([[torch.cos(theta), -torch.sin(theta)], [torch.sin(theta), torch.cos(theta)]]).to(torch.float64)
+    user_annotation = R_theta_inv @ user_annotation
+    user_annotation[0, :] = -user_annotation[0, :] + dividing_col # 1-based indexing for dividing_col 
+    user_annotation[1, :] = -user_annotation[1, :]
 
 if "virtual" in cam_label:
     epipolar_line_unlabelled, epipolar_line_labelled = get_epipolar_line(arena, user_annotation, cam_label)
+    if rotmat:
+        user_annotation = R_theta @ user_annotation
+        epipolar_line_unlabelled = R_theta @ epipolar_line_unlabelled
+        epipolar_line_unlabelled[1, :] = -epipolar_line_unlabelled[1, :] + image_width - 1
+        epipolar_line_unlabelled[0, :] *= -1
+        epipolar_line_labelled = R_theta @ epipolar_line_labelled
+        epipolar_line_labelled[1, :] = -epipolar_line_labelled[1, :] + image_width - 1
+        epipolar_line_labelled[0, :] *= -1
     epipolar_line_unlabelled = epipolar_line_unlabelled.numpy()
     epipolar_line_labelled = epipolar_line_labelled.numpy()
 
 elif "real" in cam_label:
-    user_annotation[:, 0] += dividing_column
     epipolar_line_unlabelled = get_epipolar_line(arena, user_annotation, cam_label)
     epipolar_line_unlabelled = epipolar_line_unlabelled.numpy()
     epipolar_line_labelled = np.array([])
