6574ebd550c8da65d7f44e1536703a253f06bebe
AniketRavan <aniket.lnl@gmail.com>
2025-09-14 16:01:30 -0400

    Added a bash script that recalibrates all experiments based on 6-d rotation representation. 1000 epochs run for ~1.5 hours

---
+++ recalibrate_all_exp.sh
@@ -0,0 +1,30 @@
+bsub -n 4 -J "recalibrate" -o recalibrate.log<< 'EOF'
+#!/bin/bash
+conda activate
+source /groups/branson/bransonlab/aniket/pytorch_remote/bin/activate
+
+exp_data_dir='/groups/branson/bransonlab/aniket/fly_walk_imaging/prism_new_led'
+for dir in ${exp_data_dir}/exp_*; do
+    # Check if directory exists (handles case where no exp_* directories exist)
+    if [ ! -d "$dir" ]; then
+	echo "Did not find $dir"
+        continue
+    fi
+    
+    # Check if the calibration file exists in this directory
+    calibration_image_0=($dir/calibration_grid_images/initialization_cam_0.*)
+    calibration_image_1=($dir/calibration_grid_images/initialization_cam_1.*)
+    
+    
+    if [ -f "$calibration_image_0" ] && [ -f "$calibration_image_1" ]; then
+        # Remove trailing slash for cleaner path
+        clean_dir="${dir%/}"
+        
+	exp_name=$(basename $dir)
+        echo "Processing directory: $exp_name"
+	python runme_train_simulator_camera_prism_grid_distances.py --exp_id $exp_name
+    else
+	echo "Did not find: $dir"
+    fi
+done
+EOF
